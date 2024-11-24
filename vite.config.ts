2d98b51100baee067e64447427ab9d9536a37810
Gelila-pragmatic <gelila.hamid@pragmaticdlt.com>
2024-11-24 21:08:38 +0300

    Update vite.config.ts add port

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   server: {
+    host: "0.0.0.0", // Bind to 0.0.0.0
+    port: 3000, // Use the PORT environment variable, or default to 3000
+  },
 })
